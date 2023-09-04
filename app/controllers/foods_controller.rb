e7c4b4d4f31fffac7adc4a3a1028d3c1522f7073
mohametalmeari <mohamet.almeari@gmail.com>
2023-09-04 17:17:43 +0300

    Implement Create/Destroy methods for food list

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -1,7 +1,33 @@
 class FoodsController < ApplicationController
   def index
+    @foods = Food.all.order(created_at: :desc)
   end
 
   def new
+    @food = Food.new
+  end
+
+  def create
+    @food = Food.new(food_params)
+    @food.user = current_user
+    if @food.save
+      flash[:notice] = 'New Food is added to the list!'
+      redirect_to foods_path
+    else
+      flash[:notice] = @food.errors.full_messages.join(', ')
+      redirect_to request.referrer
+    end
+  end
+  
+  def destroy
+    @food = Food.find(params[:id])
+    @food.destroy
+    redirect_to foods_path
+  end
+  
+  private 
+  
+  def food_params
+    params.require(:food).permit(:name, :measurment_unit, :price, :quantity)
   end
 end
