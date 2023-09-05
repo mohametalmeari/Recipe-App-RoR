8396cd1ad95d9bd7e845edf96648a7febdb209e0
mohametalmeari <mohamet.almeari@gmail.com>
2023-09-05 23:37:10 +0300

    Add models validations

--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -1,4 +1,6 @@
 class Ingredient < ApplicationRecord
+  validates :quantity, presence: true, numericality: { greater_than_or_equal_to: 0 }
+
   belongs_to :food
   belongs_to :recipe
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  validates :name, presence: true, length: { maximum: 100 }
   has_many :foods, foreign_key: 'user_id'
   has_many :recipes, foreign_key: 'user_id'
   # Include default devise modules. Others available are:
